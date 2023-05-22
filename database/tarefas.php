829cca499543416253cb494fa38821ddb108065e
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-05-22 16:48:15 -0300

    start/stop

--- database/tarefas.php
+++ database/tarefas.php
@@ -5,12 +5,13 @@
 
 include_once('../conexao.php');
 
-function buscaTarefas($idDemanda = null)
+function buscaTarefas($idDemanda=null,$idTarefa=null)
 {
 
     $tarefas = array();
     $apiEntrada = array(
         'idDemanda' => $idDemanda,
+        'idTarefa' => $idTarefa,
     );
     $tarefas = chamaAPI(null, '/api/services/tarefas', json_encode($apiEntrada), 'GET');
     return $tarefas;
@@ -43,8 +44,37 @@ if (isset($_GET['operacao'])) {
         $tarefas = chamaAPI(null, '/api/services/tarefas', json_encode($apiEntrada), 'PUT');
     }
 
+    if ($operacao == "start") {
+        $apiEntrada = array(
+            'tituloTarefa' => $_POST['tituloTarefa'],
+            'idCliente' => $_POST['idCliente'],
+            'idDemanda' => $_POST['idDemanda'],
+            'idAtendente' => $_POST['idAtendente'],
+            'idStatus' => $_POST['idStatus']
+        );
+        $tarefas = chamaAPI(null, '/api/services/tarefas/start', json_encode($apiEntrada), 'PUT');
+    }
+
+    if ($operacao == "alterar") {
+        $apiEntrada = array(
+            'idTarefa' => $_POST['idTarefa'],
+            'tituloTarefa' => $_POST['tituloTarefa'],
+            'dataExecucaoInicio' => $_POST['dataExecucaoInicio'],
+            'dataExecucaoFinal' => $_POST['dataExecucaoFinal']
+        );
+        $tarefas = chamaAPI(null, '/api/services/tarefas', json_encode($apiEntrada), 'POST');
+    }
+
+    if ($operacao == "stop") {
+        $apiEntrada = array(
+            'idTarefa' => $_POST['idTarefa'],
+            'dataExecucaoInicio' => $_POST['dataExecucaoInicio']
+        );
+        $tarefas = chamaAPI(null, '/api/services/tarefas/stop', json_encode($apiEntrada), 'POST');
+    }
+
 /*
     include "../demandas/tarefas_ok.php";
 */ 
-    header('Location: ../demandas/tarefas.php');
+    //header('Location: ../demandas/tarefas.php');
 }

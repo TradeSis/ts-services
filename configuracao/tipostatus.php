64c1e56e2193b5209d5e5fcae404a64b3e51296b
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-08-09 13:42:21 -0300

    update desenvremoto

--- configuracao/tipostatus.php
+++ configuracao/tipostatus.php
@@ -6,23 +6,23 @@ include_once(__DIR__ . '/../head.php');
 
 include_once(__DIR__ . '/../database/tipostatus.php');
 
-$tiposstatus = buscaTipoStatus();
+$tiposstatus = buscaTipoStatus(); 
 ?>
 
 
-<body class="bg-transparent">
+<body class="bg-transparent" >
     <div class="container" style="margin-top:10px">
 
-        <div class="row mt-4">
-            <div class="col-sm-8">
-                <p class="tituloTabela">Tipos de Status</p>
-            </div>
+            <div class="row mt-4">
+                <div class="col-sm-8">
+                        <p class="tituloTabela">Tipos de Status</p>
+                    </div>
 
-            <div class="col-sm-4" style="text-align:right">
-                <a href="tipostatus_inserir.php" role="button" class="btn btn-primary">Adicionar Status</a>
+                <div class="col-sm-4" style="text-align:right">
+                        <a href="tipostatus_inserir.php" role="button" class="btn btn-primary">Adicionar Status</a>
+                    </div>
+          
             </div>
-
-        </div>
 
         <div class="card shadow mt-2">
             <table class="table">
@@ -36,28 +36,21 @@ $tiposstatus = buscaTipoStatus();
 
                 <?php
                 foreach ($tiposstatus as $tipostatus) {
-                    ?>
+                ?>
                     <tr>
+                        <td class="text-center"><?php echo $tipostatus['nomeTipoStatus'] ?></td>
                         <td class="text-center">
-                            <?php echo $tipostatus['nomeTipoStatus'] ?>
-                        </td>
-                        <td class="text-center">
-                            <a class="btn btn-primary btn-sm"
-                                href="tipostatus_alterar.php?idTipoStatus=<?php echo $tipostatus['idTipoStatus'] ?>"
-                                role="button">Editar</a>
-                            <a class="btn btn-danger btn-sm"
-                                href="tipostatus_excluir.php?idTipoStatus=<?php echo $tipostatus['idTipoStatus'] ?>"
-                                role="button">Excluir</a>
-
-                            <button id="<?php echo $tipostatus['idTipoStatus'] ?>" class='btn btn-outline-warning btn-sm'
-                                onclick="popTipoStatus(<?php echo $tipostatus['idTipoStatus'] ?>)">Editar</button>
+                            <a class="btn btn-primary btn-sm" href="tipostatus_alterar.php?idTipoStatus=<?php echo $tipostatus['idTipoStatus'] ?>" role="button">Editar</a>
+                            <a class="btn btn-danger btn-sm" href="tipostatus_excluir.php?idTipoStatus=<?php echo $tipostatus['idTipoStatus'] ?>" role="button">Excluir</a>
+                            
+                            <button id="<?php echo $tipostatus['idTipoStatus'] ?>" class='btn btn-outline-warning btn-sm' onclick="popTipoStatus(<?php echo $tipostatus['idTipoStatus'] ?>)">Editar</button>
                         </td>
                     </tr>
                 <?php } ?>
 
             </table>
         </div>
-
+        
     </div>
 
 
@@ -66,40 +59,39 @@ $tiposstatus = buscaTipoStatus();
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Alterar Status</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h5 class="modal-title" id="editUsuarioModalLabel">Tipo de Status</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form id="edit-form">
                         <span id="msgAlertaErroEdit"></span>
 
-
-                        <input type="text" id="idTipoStatus" name="idTipoStatus" class="form-control" placeholder=""
-                            readonly>
+                        
+                        <input type="text"  id="idTipoStatus" name="idTipoStatus" class="form-control"  placeholder="">
                         <div class="mb-3">
-
-                            <input type="text" name="nomeTipoStatus" class="form-control" id="nomeTipoStatus"
-                                placeholder="">
+                           
+                            <input type="text" name="nomeTipoStatus" class="form-control" id="nomeTipoStatus" placeholder="">
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label class="labelForm">Atendimento(0=Atendente 1=Cliente)</label>
-                                <select class="form-control" id="mudaPosicaoPara" name="mudaPosicaoPara">
-
-                                    <option>0</option>
-                                    <option>1</option>
-                                </select>
+                        <div class="row">  
+                                <div class="col-md-6">
+                                    <label class="labelForm">Atendimento(0=Atendente 1=Cliente)</label>
+                                    <select class="form-control" id="mudaPosicaoPara" name="mudaPosicaoPara">
+                                       
+                                        <option>0</option>
+                                        <option>1</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <label class="labelForm">Situação (0=Fechado 1=Aberto)</label>
+                                    <select class="form-control" id="mudaStatusPara" name="mudaStatusPara">
+                                       
+                                        <option>0</option>
+                                        <option>1</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <label class="labelForm">Situação (0=Fechado 1=Aberto)</label>
-                                <select class="form-control" id="mudaStatusPara" name="mudaStatusPara">
-
-                                    <option>0</option>
-                                    <option>1</option>
-                                </select>
-                            </div>
-                        </div>
                         <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Fechar</button>
                             <input type="submit" class="btn btn-outline-warning btn-sm" id="edit-btn" value="Salvar" />
                         </div>
 
@@ -118,27 +110,27 @@ $tiposstatus = buscaTipoStatus();
     const popTipoStatusModal = new bootstrap.Modal(document.getElementById("popTipoStatus"));
 
     // Logica para Visualizar via Modal
-    async function popTipoStatus(idTipoStatus) {
-
+    async function popTipoStatus(idTipoStatus){
+        
         const dados = await fetch("<?php echo URLROOT ?>/services/database/tipostatus.php?operacao=GET_JSON&idTipoStatus=" + idTipoStatus);
         const resposta = await dados.json();
         //const popTipoStatus = new bootstrap.Modal(document.getElementById("popTipoStatus"));
         popTipoStatusModal.show();
         document.getElementById("idTipoStatus").innerHTML = resposta.idTipoStatus;
-        //  document.getElementById("nomeTipoStatus").innerHTML = resposta.nomeTipoStatus;
-        // document.getElementById("mudaPosicaoPara").innerHTML = resposta.mudaPosicaoPara;
-
+      //  document.getElementById("nomeTipoStatus").innerHTML = resposta.nomeTipoStatus;
+       // document.getElementById("mudaPosicaoPara").innerHTML = resposta.mudaPosicaoPara;
+       
         document.getElementById("idTipoStatus").value = resposta.idTipoStatus;
         document.getElementById("nomeTipoStatus").value = resposta.nomeTipoStatus;
         document.getElementById("mudaPosicaoPara").value = resposta.mudaPosicaoPara;
         document.getElementById("mudaStatusPara").value = resposta.mudaStatusPara;
 
     }
-
-
-
+     
+    
+    
     editForm.addEventListener("submit", async (e) => {
-
+    
         e.preventDefault();
 
         document.getElementById("edit-btn").value = "Salvando...";
@@ -148,14 +140,14 @@ $tiposstatus = buscaTipoStatus();
         //console.log(str);
         //console.log(editForm);
         //console.log(dadosForm);
-
+        
         /*for (var dadosFormEdit of dadosForm.entries()){
             console.log(dadosFormEdit[0] + " - " + dadosFormEdit[1]);
         }*/
 
         const dados = await fetch("<?php echo URLROOT ?>/services/database/tipostatus.php?operacao=JSON_alterar", {
             method: "POST",
-            body: dadosForm
+            body:dadosForm
         });
 
         const resposta = await dados.json();
--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $nivelMenu   =  $nivelMenuUsuario['nivelMenu'];
 
 
                 <?php
-                    $tab = '';
+                    $tab = 'dashboard'; // default
 
                     if (isset($_GET['tab'])) {$tab = $_GET['tab'];}
                
@@ -65,13 +65,6 @@ $nivelMenu   =  $nivelMenuUsuario['nivelMenu'];
                         role="tab"                        
                         style="color:black">Agenda</a>
                 </li>
-            <?php } if ($nivelMenu>=3) { ?>
-                <li class="nav-item ">
-                    <a class="nav-link <?php if ($tab=="tarefas") {echo " active ";} ?>" 
-                        href="?tab=tarefas" 
-                        role="tab"                        
-                        style="color:black">Tarefas</a>
-                </li>
             <?php } if ($nivelMenu>=4) { ?>
                 <li class="nav-item ">
                     <a class="nav-link <?php if ($tab=="configuracao") {echo " active ";} ?>" 
@@ -97,8 +90,7 @@ $nivelMenu   =  $nivelMenuUsuario['nivelMenu'];
 
     if ($tab=="demandas") {$src="demandas/";}
     if ($tab=="contratos") {$src="contratos/";}
-    if ($tab=="tarefas") {$src="demandas/tarefas.php";}
-    if ($tab=="dashboard") {$src="demandas/dashboard.php";}
+    if ($tab=="dashboard") {$src="demandas/tarefas.php";}
     if ($tab=="agenda") {$src="demandas/agenda.php";}
     if ($tab=="configuracao") {
             $src="configuracao/";

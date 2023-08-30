c4b813fe849f727aeadc0f0d248a8b95d51c75c5
Lucas Rosa <lucas.rosa@tradesis.com.br>
2023-08-30 15:48:29 -0300

    adicionado log

--- app/1/tipostatus_inserir.php
+++ app/1/tipostatus_inserir.php
@@ -2,6 +2,28 @@
 //gabriel 06022023 16:52
 //echo "-ENTRADA->".json_encode($jsonEntrada)."\n";
 
+//LOG
+$LOG_CAMINHO=defineCaminhoLog();
+if (isset($LOG_CAMINHO)) {
+    $LOG_NIVEL=defineNivelLog();
+    $identificacao=date("dmYHis")."-PID".getmypid()."-"."contrato_inserir";
+    if(isset($LOG_NIVEL)) {
+        if ($LOG_NIVEL>=1) {
+            $arquivo = fopen(defineCaminhoLog()."services_".date("dmY").".log","a");
+        }
+    }
+    
+}
+if(isset($LOG_NIVEL)) {
+    if ($LOG_NIVEL==1) {
+        fwrite($arquivo,$identificacao."\n");
+    }
+    if ($LOG_NIVEL>=2) {
+        fwrite($arquivo,$identificacao."-ENTRADA->".json_encode($jsonEntrada)."\n");
+    }
+}
+//LOG
+
 $idEmpresa = null;
 	if (isset($jsonEntrada["idEmpresa"])) {
     	$idEmpresa = $jsonEntrada["idEmpresa"];
@@ -12,17 +34,34 @@ if (isset($jsonEntrada['nomeTipoStatus'])) {
     $mudaPosicaoPara = $jsonEntrada['mudaPosicaoPara'];
     $mudaStatusPara = $jsonEntrada['mudaStatusPara'];
     $sql = "INSERT INTO tipostatus (nomeTipoStatus, mudaPosicaoPara, mudaStatusPara) values ('$nomeTipoStatus', $mudaPosicaoPara, $mudaStatusPara)";
-    if ($atualizar = mysqli_query($conexao, $sql)) {
+
+    //LOG
+    
+    //TRY-CATCH
+    try {
+
+        $atualizar = mysqli_query($conexao, $sql);
+        if (!$atualizar)
+         throw New Exception(mysqli_error($conexao));
+
         $jsonSaida = array(
             "status" => 200,
             "retorno" => "ok"
         );
-    } else {
+
+    } catch (Exception $e){
         $jsonSaida = array(
             "status" => 500,
-            "retorno" => "erro no mysql"
+            "retorno" => $e->getMessage()
         );
+        if ($LOG_NIVEL>=1) {
+            fwrite($arquivo,$identificacao."-ERRO->".$e->getMessage()."\n");
+        }
+
+    } finally {
+        // ACAO EM CASO DE ERRO (CATCH), que mesmo assim precise
     }
+    //TRY-CATCH
 } else {
     $jsonSaida = array(
         "status" => 400,

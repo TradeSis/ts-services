70ae7cc34d1fbe053314ad51b594054ef24cea1e
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-08-31 13:46:06 -0300

    Update tarefas.php

--- app/1/tarefas.php
+++ app/1/tarefas.php
@@ -3,7 +3,8 @@
 //gabriel 07022023 16:25
 //echo "-ENTRADA->".json_encode($jsonEntrada)."\n";
 
-//LOG
+/*
+//LOG 
 $LOG_CAMINHO = defineCaminhoLog();
 if (isset($LOG_CAMINHO)) {
   $LOG_NIVEL = defineNivelLog();
@@ -22,7 +23,7 @@ if (isset($LOG_NIVEL)) {
   if ($LOG_NIVEL >= 2) {
     fwrite($arquivo, $identificacao . "-ENTRADA->" . json_encode($jsonEntrada) . "\n");
   }
-}
+} */
 //LOG
 
 $idEmpresa = null;
@@ -108,13 +109,14 @@ if (isset($jsonEntrada["periodo"])) {
 //echo "-SQL->".json_encode($sql)."\n";
 $rows = 0;
 
-//LOG
+/*//LOG
 if (isset($LOG_NIVEL)) {
   if ($LOG_NIVEL >= 3) {
     fwrite($arquivo, $identificacao . "-SQL->" . $sql . "\n");
   }
 }
 //LOG
+*/
 
 //TRY-CATCH
 try {
@@ -148,7 +150,7 @@ try {
 
 
 
-
+/*
 //LOG
 if (isset($LOG_NIVEL)) {
   if ($LOG_NIVEL >= 2) {
@@ -157,6 +159,6 @@ if (isset($LOG_NIVEL)) {
 }
 //LOG
 
-
+*/
 
 ?>

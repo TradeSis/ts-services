1d831354ea4ea1f9d01df3c5efe4452109490bc4
Helio Tradesis <helio@tradesis.com.br>
2023-09-04 17:47:18 -0300

    head title

--- head.php
+++ head.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['LAST_ACTIVITY']) || !isset($_SESSION['usuario'])) {
         echo "<script>top.window.location = '" . URLROOT . "/sistema/login.php'</script>";
 }
 
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (2 * 60 * 60))) { // 60segundos * MINUTOS * HORAS
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > ($_SESSION['timeSessao'] * 60 * 60))) { // 60segundos * MINUTOS * HORAS
         session_unset();
         session_destroy();
         echo "<script>top.window.location = '" . URLROOT . "/sistema/login.php'</script>";
@@ -25,10 +25,6 @@ $logado = $_SESSION['usuario'];
 
 
 ?>
-
-
-<!DOCTYPE html>
-<html>
 
 <?php
         include_once ROOT. "/vendor/vendor.php";

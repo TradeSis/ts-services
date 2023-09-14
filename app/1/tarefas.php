668a68b1f5f941e41d4c1cfb25585c5c4fa955a6
Helio Tradesis <helio@tradesis.com.br>
2023-09-14 17:34:40 -0300

    tarefas

--- app/1/tarefas.php
+++ app/1/tarefas.php
@@ -66,7 +66,7 @@ if (isset($jsonEntrada["idAtendente"])) {
   $where = " and ";
 }
 
-if (isset($jsonEntrada["statusTarefa"])) {
+if (isset($jsonEntrada["statusTarefa"]) && $jsonEntrada["Periodo"] != 0) {
   if ($jsonEntrada["statusTarefa"] == 1) {
     $sql = $sql . $where . " tarefa.horaFinalReal IS NULL";
     $where = " and ";
@@ -82,29 +82,29 @@ if (isset($jsonEntrada["tituloTarefa"])) {
   $where = " and ";
 }
 
-if (isset($jsonEntrada["buscaTarefa"])) {
-  $sql = $sql . $where . " tarefa.idTarefa= " .  $jsonEntrada["buscaTarefa"] . " or tarefa.tituloTarefa like " . "'%" . $jsonEntrada["buscaTarefa"] . "%'";
-  $where = " and ";
-}
 
-if (isset($jsonEntrada["PrevistoInicio"])) {
-  $sql .= $where . " tarefa.Previsto >= '" . $jsonEntrada["PrevistoInicio"] . "'";
-  $where = " and ";
-}
+if (isset($jsonEntrada["Periodo"])) {
+  if ($jsonEntrada["Periodo"] == 1) {
+    if (isset($jsonEntrada["PeriodoInicio"])) {
+      $sql .= $where . " tarefa.Previsto >= '" . $jsonEntrada["PeriodoInicio"] . "'";
+      $where = " and ";
+    }
+    if (isset($jsonEntrada["PeriodoFim"])) {
+      $sql .= $where . " tarefa.Previsto <= '" . $jsonEntrada["PeriodoFim"] . "'";
+      $where = " and ";
+    }
+  }
+  if ($jsonEntrada["Periodo"] == 0) {
+    if (isset($jsonEntrada["PeriodoInicio"])) {
+      $sql .= $where . " tarefa.dataReal >= '" . $jsonEntrada["PeriodoInicio"] . "'";
+      $where = " and ";
+    }
+    if (isset($jsonEntrada["PeriodoFim"])) {
+      $sql .= $where . " tarefa.dataReal <= '" . $jsonEntrada["PeriodoFim"] . "'";
+      $where = " and ";
+    }
 
-if (isset($jsonEntrada["PrevistoFinal"])) {
-  $sql .= $where . " tarefa.Previsto <= '" . $jsonEntrada["PrevistoFinal"] . "'";
-  $where = " and ";
-}
-
-if (isset($jsonEntrada["RealInicio"])) {
-  $sql .= $where . " tarefa.dataReal >= '" . $jsonEntrada["RealInicio"] . "'";
-  $where = " and ";
-}
-
-if (isset($jsonEntrada["RealFinal"])) {
-  $sql .= $where . " tarefa.dataReal <= '" . $jsonEntrada["RealFinal"] . "'";
-  $where = " and ";
+  }
 }
 
 $order = " ORDER BY ";

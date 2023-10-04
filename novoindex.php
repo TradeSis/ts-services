72d4afe10f6fd43dafa98bcd8387aecdfb7ba479
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-04 14:26:55 -0300

    modificado tabs e perfil
    - modificado index, para ajustar o campo de perfil

--- novoindex.php
+++ novoindex.php
@@ -1,8 +1,8 @@
 <?php
 include_once __DIR__ . "/../config.php";
 include_once "novohead.php";
-include_once ROOT . "/sistema/database/loginAplicativo.php";
 include_once ROOT . "/sistema/novopainel.php";
+include_once ROOT . "/sistema/database/loginAplicativo.php";
 $nivelMenuLogin = buscaLoginAplicativo($_SESSION['idLogin'], 'Services');
 $configuracao = 1;
 $nivelMenu = $nivelMenuLogin['nivelMenu'];
@@ -13,52 +13,10 @@ $url = $_SERVER['QUERY_STRING'];
 
 
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-6 col-md-8 ">
-        </div>
-        <div class="col-6 col-md-4" >
-            <h5 style="text-align:right">PERFIL</h5>
-        </div>
-    </div>
 
-    <select class="form-select d-md-none position-sticky" id="linksub" style="color:#000; width:200px;text-align:center; margin-left:auto;margin-right:auto;margin-top:-30px">         
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=dashboard" 
-            <?php if ($url == "tab=dashboard") {echo " selected ";} ?>
-        >Dashboard</option>
-
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=agenda" 
-            <?php if ($url == "tab=agenda") {echo " selected ";} ?>
-        >Agenda</option>
-
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=execucao" 
-            <?php if ($url == "tab=execucao") {echo " selected ";} ?>
-        >Execução</option>
-        
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=demandas" 
-            <?php if ($url == "tab=demandas") {echo " selected ";} ?>
-        >Demandas</option>
-
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=contratos" 
-            <?php if ($url == "tab=contratos") {echo " selected ";} ?>
-        >Contratos</option>
-
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=projetos" 
-            <?php if ($url == "tab=projetos") {echo " selected ";} ?>
-        >Projetos</option>
-
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=os" 
-            <?php if ($url == "tab=os") {echo " selected ";} ?>
-        >O.S.</option>
-
-        <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=configuracao" 
-            <?php if ($url == "tab=configuracao") {echo " selected ";} ?>
-        >Configurações</option>
-    </select>
-
-    <div class="row border-bottom d-none d-md-block fixed-top">
-        <div class="col-md-12 d-flex justify-content-center">
+    <div class="row ">
+        <div class="col-md-9 d-none d-md-none d-lg-block" style="background-color: #13216A;">
             <ul class="nav a" id="myTabs">
-
 
                 <?php
                 $tab = ''; // default
@@ -78,7 +36,6 @@ $url = $_SERVER['QUERY_STRING'];
                                                         } ?>" href="?tab=dashboard" role="tab">Dashboard</a>
                     </li>
                 <?php }
-
 
                 if ($nivelMenu >= 1) {
                 ?>
@@ -131,8 +88,6 @@ $url = $_SERVER['QUERY_STRING'];
                     </li>
                 <?php }
 
-
-
                 if ($nivelMenu >= 4) { ?>
                     <li class="nav-item mr-1">
                         <a class="nav-link1 nav-link <?php if ($tab == "configuracao") {
@@ -143,11 +98,61 @@ $url = $_SERVER['QUERY_STRING'];
 
 
             </ul>
+        </div>
+        <!--Essa coluna só vai aparecer em dispositivo mobile-->
+        <div class="col-7 col-md-9 d-md-block d-lg-none" style="background-color: #13216A;">
+            <select class="form-select mt-2" id="linksub" style="color:#000; width:160px;text-align:center; ">
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=dashboard" <?php if ($url == "tab=dashboard") {
+                                                                                                echo " selected ";
+                                                                                            } ?>>Dashboard</option>
 
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=agenda" <?php if ($url == "tab=agenda") {
+                                                                                            echo " selected ";
+                                                                                        } ?>>Agenda</option>
+
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=execucao" <?php if ($url == "tab=execucao") {
+                                                                                                echo " selected ";
+                                                                                            } ?>>Execução</option>
+
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=demandas" <?php if ($url == "tab=demandas") {
+                                                                                                echo " selected ";
+                                                                                            } ?>>Demandas</option>
+
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=contratos" <?php if ($url == "tab=contratos") {
+                                                                                                echo " selected ";
+                                                                                            } ?>>Contratos</option>
+
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=projetos" <?php if ($url == "tab=projetos") {
+                                                                                                echo " selected ";
+                                                                                            } ?>>Projetos</option>
+
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=os" <?php if ($url == "tab=os") {
+                                                                                        echo " selected ";
+                                                                                    } ?>>O.S.</option>
+
+                <option value="<?php echo URLROOT ?>/services/novoindex.php?tab=configuracao" <?php if ($url == "tab=configuracao") {
+                                                                                                    echo " selected ";
+                                                                                                } ?>>Configurações</option>
+            </select>
+        </div>
+        <div class="col-5 col-md-3 " style="text-align:right;background-color: #13216A;">
+
+            <button class="btn text-white  position-relative dropdown-toggle mt-2 mr-3" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="bi bi-person-fill"></i>&#32;Lucas
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    99+
+                    <span class="visually-hidden">unread messages</span>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Mensagens</a>
+                <a class="dropdown-item" href="#">Perfil</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Logout</a>
+            </div>
 
         </div>
-
     </div>
+
 
     <?php
     $src = "";
@@ -200,7 +205,6 @@ $url = $_SERVER['QUERY_STRING'];
     if ($src !== "") {
 
     ?>
-
         <div class="container-fluid p-0 m-0" style="overflow: hidden;">
             <iframe class="row p-0 m-0" id="iFrameTab" style="width: 100%; height: 86vh; border:none" src="<?php echo URLROOT ?>/services/<?php echo $src ?>"></iframe>
         </div>
@@ -208,7 +212,7 @@ $url = $_SERVER['QUERY_STRING'];
     }
     ?>
 
-</div>
+</div><!--div container-->
 
 <!-- temporario -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -225,3 +229,7 @@ $url = $_SERVER['QUERY_STRING'];
         });
     });
 </script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>

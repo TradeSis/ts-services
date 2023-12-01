44cf224a360b575d7c0aa5f8e21ed2dde06f35ad
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-12-01 13:36:44 -0300

    Update demanda_alterar

--- app/1/demanda_alterar.php
+++ app/1/demanda_alterar.php
@@ -46,15 +46,28 @@ if (isset($jsonEntrada['idDemanda'])) {
     $idAtendente = $jsonEntrada['idAtendente'];
     $dataPrevisaoEntrega  = isset($jsonEntrada['dataPrevisaoEntrega'])  && $jsonEntrada['dataPrevisaoEntrega'] !== "" && $jsonEntrada['dataPrevisaoEntrega'] !== "null" ? "'". $jsonEntrada['dataPrevisaoEntrega']."'"  : "null";
     $dataPrevisaoInicio  = isset($jsonEntrada['dataPrevisaoInicio'])  && $jsonEntrada['dataPrevisaoInicio'] !== "" && $jsonEntrada['dataPrevisaoInicio'] !== "null" ? "'". $jsonEntrada['dataPrevisaoInicio']."'"  : "null";
-    $tempoCobrado = isset($jsonEntrada["tempoCobrado"])  && $jsonEntrada["tempoCobrado"] !== "" && $jsonEntrada["tempoCobrado"] !== "null" ? "'". $jsonEntrada["tempoCobrado"]."'"  : "null";
+    $tempoCobradoEntrada = isset($jsonEntrada["tempoCobrado"])  && $jsonEntrada["tempoCobrado"] !== "" && $jsonEntrada["tempoCobrado"] !== "null" ? "'". $jsonEntrada["tempoCobrado"]."'"  : "null";
 
-    $tempoCobradoDigitado = '0';
-    if($tempoCobrado !== "null"){
-        $tempoCobradoDigitado = '1';
-    }
-    $sql = "UPDATE demanda SET prioridade = $prioridade, tituloDemanda = $tituloDemanda, descricao = $descricao, idServico = $idServico, idAtendente = $idAtendente,
-             horasPrevisao = $horasPrevisao, idContrato = $idContrato, dataPrevisaoEntrega = $dataPrevisaoEntrega, dataPrevisaoInicio = $dataPrevisaoInicio, tempoCobrado = $tempoCobrado, tempoCobradoDigitado = $tempoCobradoDigitado, dataAtualizacaoAtendente=CURRENT_TIMESTAMP() WHERE idDemanda = $idDemanda";
+    //Busca tempoCObrado de Demanda  
+    $sql_consulta = "SELECT * FROM demanda WHERE idDemanda = $idDemanda";
+    $buscar_consulta = mysqli_query($conexao, $sql_consulta);
+    $row_consulta = mysqli_fetch_array($buscar_consulta, MYSQLI_ASSOC);
+    $tempoCobradoAutal = $row_consulta["tempoCobrado"];
+    $tempoCobradoAutal = "'". $tempoCobradoAutal . "'";
    
+   $sql = "UPDATE demanda SET prioridade = $prioridade, tituloDemanda = $tituloDemanda, descricao = $descricao, idServico = $idServico, idAtendente = $idAtendente,
+   horasPrevisao = $horasPrevisao, idContrato = $idContrato, dataPrevisaoEntrega = $dataPrevisaoEntrega, dataPrevisaoInicio = $dataPrevisaoInicio ";
+
+    if ($tempoCobradoEntrada != $tempoCobradoAutal) {
+        $tempoCobrado = $tempoCobradoEntrada;
+        $tempoCobradoDigitado = '1';
+       
+       $sql = $sql . ",tempoCobrado = $tempoCobrado, tempoCobradoDigitado = $tempoCobradoDigitado ";
+    }
+
+    $sql = $sql . ", dataAtualizacaoAtendente=CURRENT_TIMESTAMP()  WHERE idDemanda = $idDemanda";
+
+  
     //LOG
     if (isset($LOG_NIVEL)) {
         if ($LOG_NIVEL >= 3) {

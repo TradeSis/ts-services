80fef3e5d2c5ef55654ed63225ae4037ca60062c
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-12-08 14:19:47 -0300

    Revert "modelo de descricao e comentario - modal"
    This reverts commit bb4cfeef852b3e2b4ee8e1d3f7cc62503d343d60.

--- demandas/demanda_descricao.php
+++ demandas/demanda_descricao.php
@@ -1,42 +1,24 @@
-
-<div class="col text-end">
-
-
-    <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#descricaoModal">
-        Editar
-    </button>
-</div>
-<div class="container-fluid p-0 ts-containerDescricaoDemanda">
+<form action="../database/demanda.php?operacao=descricao" method="post">
+    <div class="col-md-12">
+        <div class="container-fluid p-0 ts-containerDescricaoDemanda">
             <div class="row">
-       
+                <div class="col">
+                <span class="tituloEditor">Descrição</span>
+                </div>
+                <div class="col text-end">
+                <a class="ts-btnDescricaoEditar"><i class="bi bi-pen"></i>&#32;Editar</a>
+                </div>
+            </div>
+          
             <div class="quill-textarea ts-displayDisable" style="height: 30vh!important;"><?php echo $demanda['descricao'] ?></div>
             <textarea style="display: none" id="quill-descricao" name="descricao"><?php echo $demanda['descricao'] ?></textarea>
             <input type="hidden" class="form-control ts-input" name="idDemanda" value="<?php echo $demanda['idDemanda'] ?>">
         </div>
-
-<!-- Modal -->
-<div class="modal fade" id="descricaoModal" tabindex="-1" aria-labelledby="descricaoModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Descrição</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="../database/demanda.php?operacao=descricao" method="post">
-                    <div class="container-fluid p-0">
-                        <div class="quill-textarea2" style="height: 30vh!important;"><?php echo $demanda['descricao'] ?></div>
-                        <textarea style="display: none" id="quill-descricao2" name="descricao"><?php echo $demanda['descricao'] ?></textarea>
-                        <input type="hidden" class="form-control ts-input" name="idDemanda" value="<?php echo $demanda['idDemanda'] ?>">
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-success">Salvar</button>
-            </div>
-            </form>
-        </div>
+    </div><!--col-md-6-->
+    <div class="col text-end">
+        <button type="submit" class="btn btn-success mt-1 btnSalvarComentario ts-sumir">Salvar</button>
     </div>
-</div>
+</form>
 
 <?php include_once 'comentarios.php'; ?>
 
@@ -48,10 +30,10 @@
         $('.quill-textarea').toggleClass('ts-displayDisable');
         $('.btnSalvarComentario').toggleClass('ts-sumir');
         $('.ql-toolbar').show();
-    });
+    }); 
 
     $('.btnAdicionarComentario').click(function() {
-        $('.containerComentario').toggleClass('ts-sumir');
+        $('.containerComentario').toggleClass('ts-sumir'); 
         $('.ts-inputComentario').addClass('ts-sumir');
     });
 </script>

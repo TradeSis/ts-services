ba3dfa05418c27950a60fd73b221e741c884ba06
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-12-29 14:15:38 -0300

    adicionado function comparaDatas

--- demandas/index.php
+++ demandas/index.php
@@ -393,21 +393,29 @@ if (isset($_SESSION['filtro_demanda'])) {
            
             var dataFechamento = new Date(object.dataFechamento);
             dataFechamentoFormatada = dataFechamento.toLocaleDateString("pt-BR");
-
+           
             dataPrevisaoEntrega = object.dataPrevisaoEntrega
             if(object.dataPrevisaoEntrega == null){
               dataPrevisaoEntrega = "null"
             }
             var dataEntrega = new Date(dataPrevisaoEntrega);
             dataPrevisaoEntregaFormatada = (`${dataEntrega.getUTCDate().toString().padStart(2, '0')}/${(dataEntrega.getUTCMonth()+1).toString().padStart(2, '0')}/${dataEntrega.getUTCFullYear()}`);
-                 
+      
             if(object.prioridade == '99'){
               object.prioridade = ''
             }
 
-            const date = new Date();
-            dataAtual = (`${date.getUTCDate().toString().padStart(2, '0')}/${(date.getUTCMonth()+1).toString().padStart(2, '0')}/${date.getUTCFullYear()}`);
-         
+            function comparaDatas (date) {
+              let parts = date.split('/')
+              let today = new Date()     
+              date = new Date(parts[2], parts[1] - 1, parts[0]) 
+
+              return date < today ? true : false
+            }
+
+            datacomparacao =  comparaDatas (dataPrevisaoEntregaFormatada);
+
+
             linha += "<tr>";  
             /* helio 09112023 - classe ts-click para quando clicar,
                data-idDemanda para guardar o id da demanda */
@@ -428,11 +436,10 @@ if (isset($_SESSION['filtro_demanda'])) {
             datas += "<td class='ts-click' data-idDemanda='" + object.idDemanda + "'>" + object.nomeServico + "</td>";
             datas += "<td class='ts-click' data-idDemanda='" + object.idDemanda + "'" 
             
-            if(dataPrevisaoEntregaFormatada != "NaN/NaN/NaN"){
-              if((dataPrevisaoEntregaFormatada > dataAtual) && (object.dataFechamento == null)){
+            if((datacomparacao == true) && (object.dataFechamento == null)){
               datas += " style='background:firebrick;color:white'";
-                }
             }
+            
             
             datas += ">" + 'Abertura: ' + dataAberturaFormatada + '<br>' 
             if (object.dataPrevisaoEntrega == null) {
